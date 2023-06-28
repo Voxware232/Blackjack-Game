f357b5ad4e78cfaf5ce72f7a1f27bef1ece8861d
Voxware232 <66388586+Voxware232@users.noreply.github.com>
2023-06-28 23:16:19 +0100

    Update Files

---
+++ index.js
@@ -0,0 +1,37 @@
+let firstCard = 10;
+let secondCard = 3;
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+function startGame() {
+  renderGame();
+}
+function renderGame() {
+  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+  sumEl.textContent = "Sum: " + sum;
+  if (sum < 21) {
+    message = "Do you want to draw a new card? 🙂";
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game! 😭";
+    isAlive = false;
+  }
+  console.log(message);
+  messageEl.textContent = message;
+}
+function newCard() {
+  let newCard = cards[2];
+  newCard = 3;
+
+  sum += newCard;
+
+  renderGame();
+}
