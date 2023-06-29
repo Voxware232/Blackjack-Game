1bd88fc751165d6f0aec161d3ef2205ce018635d
Voxware232 <66388586+Voxware232@users.noreply.github.com>
2023-06-29 23:26:58 +0100

    Final Base Update

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
-let firstCard = 10;
-let secondCard = 3;
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards=[]
+let sum = 0
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el")
+
+let player = {
+  name:'Jan',
+  funds:100
+}
+
+playerEl.textContent = player.name + " :$" + player.funds;
+function getRandomCard(){
+  let randomCard = Math.floor((Math.random()*13)+1)
+  if ((randomCard === 1)) {
+    return 11;
+  } else if ((randomCard > 10)) {
+    return 10;
+  }
+  else{
+    return randomCard;
+  }
+  
+}
 
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
   renderGame();
 }
 function renderGame() {
-  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+  cardsEl.textContent = "Cards: " ;
+
+  for(let i = 0 ; i<cards.length; i++)
+  {
+    cardsEl.textContent += cards[i] + " ";
+  }
   sumEl.textContent = "Sum: " + sum;
   if (sum < 21) {
     message = "Do you want to draw a new card? 🙂";
@@ -28,13 +56,12 @@ function renderGame() {
   messageEl.textContent = message;
 }
 function newCard() {
-  let newCard = cards[2];
-  newCard = 3;
-
-  sum += newCard;
-
-  // Push new card to array
-
-  cards.push(newCard);
-  renderGame();
+  if(isAlive === true && hasBlackJack === false)
+  {
+    let newCard = getRandomCard();
+    sum += newCard;
+    // Push new card to array
+    cards.push(newCard);
+    renderGame();
+  }
 }
